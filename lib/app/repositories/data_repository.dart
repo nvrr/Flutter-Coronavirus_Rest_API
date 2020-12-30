78b0a2c2593483ca99be3797d568810403f64d80
nvrr <sureshvarma809@gmail.com>
2020-12-30 11:46:00 +0530

    Loading data from multiple endpoints and polish the UI: 1.Loading data from multiple endpoints with Future.wait

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -24,4 +24,14 @@ class DataRepository {
       rethrow;
     }
   }
+
+  Future<void> _getAllEndpointsData() async {
+    await Future.wait([
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+  }
 }
